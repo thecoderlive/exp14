2169e8671670fd0aec1c00aec5458058562cea32
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:34:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "d46ee4d7-d2d2-4251-a43e-28c200abf339",
+    "home_feed": [
+        {
+            "id": "5638c4bf-b59b-4727-ac2c-8dd2654eda36",
+            "artist_name": "send",
+            "home_carousel": [
+                {
+                    "id": "dcc9fd98-2822-41d7-93f7-f0d6f7d6a796",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                },
+                {
+                    "id": "3e50843f-620f-428e-ae56-225514b82645",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "8310938c-456a-405d-91a9-28892865fad9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                },
+                {
+                    "id": "6121aff6-03ef-4753-b4a9-6fad00631f7b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "020ab4ac-c24e-41f0-a7a2-b7d3ee5f4d30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png"
+                }
+            ],
+            "art_caption": "west",
+            "art_description": "Interview project organization soldier church. Total argue else allow scene two charge."
+        },
+        {
+            "id": "d6b85366-8247-4705-ba9c-cfe567fc7d45",
+            "artist_name": "language",
+            "home_carousel": [
+                {
+                    "id": "4d00661e-8cae-43e3-8f28-6dc32178a7d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                },
+                {
+                    "id": "c51d34e4-72d6-43c7-abfb-34437e5c2d26",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "11ac8595-6708-4727-8c0c-7c0494ed4630",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "8a6f75ae-eae0-49e3-b6bb-85ac187853b4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png"
+                },
+                {
+                    "id": "920efa08-3273-45e0-a840-f43bfd15aa99",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                }
+            ],
+            "art_caption": "girl",
+            "art_description": "Society down field art. Poor language space capital. Meet decide benefit generation walk sing."
+        },
+        {
+            "id": "96e57402-3a8d-46d1-8449-4f6bb65195fe",
+            "artist_name": "office",
+            "home_carousel": [
+                {
+                    "id": "f9bb9a5d-123f-46ce-98ca-bb8831a0153d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+                },
+                {
+                    "id": "33a1e82a-dff8-4141-ac98-3e2cfb9df357",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "4e343644-dcdb-423d-af9d-16aecfaac46b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png"
+                },
+                {
+                    "id": "d09955a8-5eb2-469c-90e2-bef15cab9dc1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                },
+                {
+                    "id": "5e8ae5f2-3be5-4244-acdf-46e5fc277c63",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                }
+            ],
+            "art_caption": "own",
+            "art_description": "Modern color way leg speech area. Still program list sign talk page owner close."
+        },
+        {
+            "id": "cf77bec4-c42a-4370-afa8-61a7673879ce",
+            "artist_name": "they",
+            "home_carousel": [
+                {
+                    "id": "0c90e0e7-ffa4-48b8-a224-2c31f4d1e240",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "4c22312b-2bec-49bf-9598-74e1ccb821e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "885bde8c-57b4-41c3-9c4e-95551b36872a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/152.png"
+                },
+                {
+                    "id": "063eb4d8-f0c4-4dd5-8c04-e1a5d57e11eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "31902195-d063-4022-b46c-6840a2525651",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                }
+            ],
+            "art_caption": "expect",
+            "art_description": "Option face hot worker debate example work. Under different ask base apply time. Able see every exist."
+        },
+        {
+            "id": "96fb8285-96c8-40a0-acfd-16d878daa365",
+            "artist_name": "clearly",
+            "home_carousel": [
+                {
+                    "id": "d957366b-458a-455e-8de5-b95a83afca2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "948730b5-c640-489d-a2c9-b9c81da4ea49",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "78cdf8d9-ae7d-4916-9eb4-b6f8e74bad15",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "2271e33b-bf9d-4a2b-b89a-f17098b8a113",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "fe54f7fa-7ce4-4bb1-a246-702d0176282c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                }
+            ],
+            "art_caption": "firm",
+            "art_description": "All marriage special state field. Brother factor become whose town himself. Arm maintain recognize white."
+        }
+    ]
+}
